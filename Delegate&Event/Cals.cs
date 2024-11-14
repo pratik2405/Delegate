8d709241a6bd22ed0a7eb08cc35b46c5a4268fc1
pratik2405 <shinde7388pratik@gmail.com>
2024-11-14 12:12:09 +0530

    Add project files.

---
+++ Delegate&Event/Cals.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Delegate_Event
+{
+    //Syntax:-
+    //Access specifier  delegate-keyword return type delegate-name(parameters…)
+    //Method syntax & delegate syntax should be same  (syntax – return type, parameters)
+
+    public delegate int MyDelegate(int a, int b);
+    public delegate string MyDelegate2(string a);
+    public class Cals
+    {
+        public int Add(int x, int y) 
+        {
+            return x + y;
+        }
+        
+        public int Sub(int x, int y) 
+        {
+            return x - y;
+        }
+        public int Mul(int x, int y) 
+        {
+            return x * y;
+        }
+    }
+
+    public class User
+    {
+        public string ChangeName(string name)
+        {
+            return name.ToUpper();
+        }
+    }
+}
---
+++ Delegate&Event/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Event
+{
+    internal class Program
+    {
+        //Delegates are reference type.
+        //Delegate used to hold an reference of method
+        //Single cast delegate  → One delegate hold one method reference
+        //Multicast delegate  → One delegate hold multiple method references
+        static void Main(string[] args)
+        {
+            Cals c=new Cals();
+
+            MyDelegate obj = new MyDelegate(c.Add);
+            // += used to append reference obj in invocation list
+            // -= used to remove reference obj from invocation list
+            obj += new MyDelegate(c.Sub); //add delegate and create multi cast delegate
+            obj += new MyDelegate(c.Mul);
+
+            //int result = obj.Invoke(20, 10); //Invoke is used to run single cast delegate
+            //Console.WriteLine(result);
+
+            // Delegate will create the invocationList 
+            // invocationList hold methods refer in the same sequence that we have added
+            // basic need of multicast delegate is --> to invoke methods in a sequence.
+            Delegate[] list = obj.GetInvocationList();
+            foreach (Delegate item in list) 
+            {
+                Console.WriteLine(item.Method);
+                Console.WriteLine(item.DynamicInvoke(30,20));
+            }
+
+            //User class
+
+            User u= new User();
+            MyDelegate2 obj2 = new MyDelegate2(u.ChangeName);
+
+            string str=obj2.Invoke("pratik");
+            Console.WriteLine(str);
+        }
+    }
+}
